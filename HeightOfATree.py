788fccc07921bde87c9bffb69716a16fc8ee4d59
vpc <cuevasvp@gmail.com>
2020-06-13 13:18:32 +0800

    first commit

---
+++ HeightOfATree.py
@@ -0,0 +1,26 @@
+from binarytree import tree
+
+
+def height(node):
+    """
+    The height of a node in a tree is the number of edges on the longest simple downward path from the node to a leaf,
+    and the height of a tree is the height of its root. The height of a tree is also equal to the largest depth of
+    any node in the tree.
+
+    :param node: node of the tree
+    :return: height of the node
+    """
+
+    left = right = 0
+    if node.left:
+        left = 1 + height(node.left)
+    if node.right:
+        right = 1 + height(node.right)
+    return max(left, right)
+
+
+if __name__ == '__main__':
+    # t = tree(height=3, is_perfect=False)
+    t = tree()
+    print(t)
+    print(height(t))
---
+++ test_HeightOfATree.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from binarytree import tree
+from random import randint
+from HeightOfATree import height
+
+
+class Test(TestCase):
+    def test_height(self):
+        for _ in range(1000):
+            h = randint(0, 9)
+            t = tree(height=h)
+            # print(t.height)
+            # print(t)
+            self.assertEqual(t.height, height(t))
