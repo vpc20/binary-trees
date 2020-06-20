9eef3c2a2931a69982b8a3eaa8753224cfe5c040
vpc <cuevasvp@gmail.com>
2020-06-20 14:35:02 +0800

    initial commit

---
+++ BinaryTreeSampleData.py
@@ -0,0 +1,30 @@
+from itertools import combinations
+
+from BinaryTrees import binary_tree
+
+arr = [4, 2, 6, 1, 3, 5, 7]
+idxs = list(range(1, len(arr)))
+print(idxs)
+tree_arr = []
+
+for r in range(1, len(arr)):
+    for comb in combinations(idxs, r):
+        arr_copy = arr.copy()
+        for idx in comb:
+            arr_copy[idx] = None
+        for i in range(len(arr_copy)):
+            if arr_copy[i] is None:  # if parent is None children should also be None
+                l = 2 * i + 1
+                if l < len(arr_copy):
+                    arr_copy[l] = None
+                r = 2 * i + 2
+                if r < len(arr_copy):
+                    arr_copy[r] = None
+        if arr_copy not in tree_arr:  # remove duplicates
+            tree_arr.append(arr_copy)
+
+tree_arr.sort(key=lambda e: [item if item is not None else 0 for item in e])
+
+for arr in tree_arr:
+    t = binary_tree(arr)
+    print(t)
