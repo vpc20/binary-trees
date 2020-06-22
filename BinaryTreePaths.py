1f64140185b884555a69d537c5a534779d321630
vpc <cuevasvp@gmail.com>
2020-06-22 13:47:32 +0800

    include bfs solution

--- BinaryTreePaths.py
+++ BinaryTreePaths.py
@@ -15,6 +15,7 @@
 # Output: ["1->2->5", "1->3"]
 #
 # Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+from collections import deque
 
 from BinaryTrees import binary_tree
 
@@ -51,6 +52,24 @@ def binary_tree_paths(node):
     return paths
 
 
-t = binary_tree([1, 2, 3, 4, 5, 6, 7])
+def binary_tree_paths_bfs(node):
+    if node is None:
+        return []
+    paths = []
+    q = deque([(node, str(node.val))])
+    while q:
+        node, path = q.popleft()
+        if node.left is None and node.right is None:
+            paths.append(path)
+        if node.left:
+            q.append((node.left, f'{path}->{node.left.val}'))
+        if node.right:
+            q.append((node.right, f'{path}->{node.right.val}'))
+    return paths
+
+
+# t = binary_tree([1, 2, 3, 4, 5, 6, 7])
+t = binary_tree([3, 1, 4, None, 2, None, 5])
 print(t)
 print(binary_tree_paths(t))
+print(binary_tree_paths_bfs(t))
